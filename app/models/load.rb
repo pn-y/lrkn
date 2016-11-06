17af5444c1c8546caee4bc8e8bba01e82a4f1f0b
Artem Skripka <therewasadrizzle@gmail.com>
2016-11-06 21:50:41 +0700

    add todo

--- app/models/load.rb
+++ app/models/load.rb
@@ -12,4 +12,7 @@ class Load < ActiveRecord::Base
     order('delivery_date DESC',
           "(case delivery_shift when 'E' then 0 when 'N' then 1 when 'M' then 2 end) ASC")
   end)
+
+  # TODO: truck availability validation
+  # TODO: refactor truck volume validation
 end
