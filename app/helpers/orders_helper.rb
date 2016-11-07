464b53a259ba899357ccd08a5dc7441e9c3051ef
Artem Skripka <therewasadrizzle@gmail.com>
2016-11-07 18:07:49 +0700

    refactor views

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -15,4 +15,38 @@ module OrdersHelper
     end
     label << "#{order.handling_unit_quantity}, #{order.handling_unit_type}, #{order.volume}"
   end
+
+  def address_string(order)
+    it_is_return = returning_order?(order)
+    if it_is_return
+      "#{order.origin_address}, #{order.origin_city}, #{order.origin_state}, #{order.origin_zip}"
+    else
+      "#{order.destination_address}, #{order.destination_city}, "\
+      "#{order.destination_state}, #{order.destination_zip}"
+    end
+  end
+
+  def cargo_string(order)
+    it_is_return = returning_order?(order)
+
+    verb = it_is_return ? 'load ' : 'unload '
+    verb + pluralize(order.handling_unit_quantity, order.handling_unit_type)
+  end
+
+  def routing_list_address_string(order)
+    it_is_return = returning_order?(order)
+    if it_is_return
+      "#{order.origin_address}, #{order.origin_city}, #{order.origin_state}, #{order.origin_zip}"
+    else
+      "#{order.destination_address}, #{order.destination_city}, "\
+      "#{order.destination_state}, #{order.destination_zip}"
+    end
+  end
+
+  def contacts_string(order)
+    it_is_return = returning_order?(order)
+
+    client_name = it_is_return ? order.origin_name : order.client_name
+    "#{client_name},  #{order.phone_number}"
+  end
 end
--- app/models/load.rb
+++ app/models/load.rb
@@ -7,6 +7,4 @@ class Load < ActiveRecord::Base
     order('delivery_date DESC',
           "(case delivery_shift when 'E' then 0 when 'N' then 1 when 'M' then 2 end) ASC")
   end)
-
-  # TODO: truck availability validation
 end
