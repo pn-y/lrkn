6fdd8592f7adc6c88b1559063abf76829b7b66f6
Artem Skripka <therewasadrizzle@gmail.com>
2016-11-15 00:21:04 +0700

    use model fields instead view helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,12 +1,7 @@
 module OrdersHelper
-  def returning_order?(order)
-    order.client_name == 'Larkin LLC'
-  end
-
   def order_label_for_load(order)
-    it_is_return = returning_order?(order)
     label = "#{order.delivery_date}, #{order.delivery_shift}"
-    if it_is_return
+    if order.returning
       label << " #{order.origin_address}, #{order.origin_city}"
       label << ", #{order.origin_state}, #{order.origin_zip}"
     else
@@ -14,13 +9,12 @@ module OrdersHelper
       label << ", #{order.destination_state}, #{order.destination_zip}"
     end
     label << "#{order.handling_unit_quantity}, #{order.handling_unit_type}, #{order.volume}"
-    label << ', Return' if it_is_return
+    label << ', Return' if order.returning
     label
   end
 
   def address_string(order)
-    it_is_return = returning_order?(order)
-    if it_is_return
+    if order.returning
       "#{order.origin_address}, #{order.origin_city}, #{order.origin_state}, #{order.origin_zip}"
     else
       "#{order.destination_address}, #{order.destination_city}, "\
@@ -30,9 +24,8 @@ module OrdersHelper
 
   def load_cargo_string(order)
     return unless order.handling_unit_quantity && order.handling_unit_type
-    it_is_return = returning_order?(order)
 
-    verb = it_is_return ? 'load ' : 'unload '
+    verb = order.returning ? 'load ' : 'unload '
     verb + cargo_string(order)
   end
 
@@ -42,8 +35,7 @@ module OrdersHelper
   end
 
   def route_list_address_string(order)
-    it_is_return = returning_order?(order)
-    if it_is_return
+    if order.returning
       "#{order.origin_address}, #{order.origin_city}, #{order.origin_state}, #{order.origin_zip}"
     else
       "#{order.destination_address}, #{order.destination_city}, "\
@@ -52,16 +44,14 @@ module OrdersHelper
   end
 
   def contacts_string(order)
-    it_is_return = returning_order?(order)
-
-    client_name = it_is_return ? order.origin_name : order.client_name
+    client_name = order.returning ? order.origin_name : order.client_name
     "#{client_name},  #{order.phone_number}"
   end
 
   def order_row_style(order)
     if order.load_id
       'info'
-    elsif returning_order?(order)
+    elsif order.returning
       'warning'
     elsif danger_order?(order)
       'danger'
